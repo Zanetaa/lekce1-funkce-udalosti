2382029a8aa6386e2a75bc982d9a0e2267926d6a
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-02-12 21:45:20 +0100

    Develop steps

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+/**
+ * Zobrazí výsledek sečtení dvou čísel. Na sečtení se využívá interní funkce.
+ * 
+ * @param {string} selector
+ * @param {int} a 
+ * @param {int} b 
+ */
+ function zobrazVysledek(selector, a, b) {
+	document.querySelector(selector).innerHTML = secti(a, b);
+}
+
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -18,4 +28,20 @@ function secti(a, b) {
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log('Gratulace, právě si spustila tuto funkci!');
+
+	let squareElement = document.getElementsByClassName('ctverecek')[0]
+	squareElement.innerHTML = 'Gratulace, právě si spustila tuto funkci!';
+	// Hmmm... dostali jsme se do problému s délkou řetězce a šířkou elementu. Pojďme to vyřešit.
+	squareElement.style.width = '400px';
 }
+
+/**
+ * Upraví barvu pozadí předaného prvku
+ * 
+ * @param {string} elementSelector 
+ */
+function zmen(elementSelector) {
+	document.querySelector(elementSelector).style.backgroundColor = 'green';
+}
+
